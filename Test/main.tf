a540ab68f44eac0fa2c9582a0a53199386de9872
Diyauddin-Shaik <167270609+Diyauddin-Shaik@users.noreply.github.com>
2024-05-24 11:35:30 +0530

    Create main.tf

---
+++ Test/main.tf
@@ -0,0 +1,24 @@
+provider "aws"{
+        alias="insta1"
+        region="us-east-1"
+}
+provider "aws"{
+        alias="insta2"
+        region="us-east-1"
+}
+resource "aws_instance" "example1"{
+        ami= "ami-04b70fa74e45c3917"
+        instance_type="t2.large"
+        provider=aws.insta1
+        tags={
+        Name="master1"
+        }
+}
+resource "aws_instance" "example2"{
+        ami= "ami-04b70fa74e45c3917"
+        instance_type="t2.large"
+        provider=aws.insta2
+        tags={
+        Name="worker"
+        }
+}
